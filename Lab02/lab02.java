d4ad2a61fcacacef70655eea19951456e4885a19
Nick Graham <nickdud341@gmail.com>
2015-01-26 11:58:41 -0500

    more updates

--- Lab02/lab02.java
+++ Lab02/lab02.java
@@ -1,32 +1,44 @@
 /*
- * Lab2
- *
- * Lab 2
- *
- * Copyright 2015
- *
- * Course: CSC 172 Spring 2015
- *
- * Assignment: Lab 2
- *
- * Author: Nicholas Graham
- *
- * Lab Session: Wednesday 6:15 - 7:30
- *
- * Lab TA: Kate Zeng Zhiming
- *
- * Last Revised: January 25, 2014.
- */
+* Lab2
+*
+* Lab 2
+*
+* Copyright 2015
+*
+* Course: CSC 172 Spring 2015
+*
+* Assignment: Lab 2
+*
+* Author: Nicholas Graham
+*
+* Lab Session: Wednesday 6:15 - 7:30
+*
+* Lab TA: Kate Zeng Zhiming
+*
+* Last Revised: January 25, 2014.
+*/
 
 public class lab02<AnyType> implements SimpleLinkedList<AnyType> {
   private MyNode start;
 
-  public lab02 (){
-    start = null;
+  public lab02(){
+    start = new MyNode();
   }
 
   public void insert(AnyType x){
-
+    MyNode e = start.next;
+    MyNode in = new MyNode();
+    in.data = x;
+    do {
+    //while (e.next != null){
+      if (e.data != null){
+        e = e.next;
+      }
+      else {
+        e = in;
+        e = e.next;
+      }
+    }while (e != null);
   }
 
   public void delete(AnyType x){
@@ -38,10 +50,19 @@ public class lab02<AnyType> implements SimpleLinkedList<AnyType> {
   }
 
   public boolean isEmpty(){
-    return true;
+    if (start.next == null){
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   public void printlist(){
-
+    MyNode e = start;
+    while (e.next != null){
+      System.out.println(e.next.data);
+      e = e.next;
+    }
   }
 }
---
+++ Lab02/test.java
@@ -0,0 +1,11 @@
+public class test {
+  public static void main (String[] args){
+    lab02 t = new lab02();
+    t.insert("h");
+    t.insert("e");
+    t.insert("l");
+    t.insert("l");
+    t.insert("o");
+    t.printlist();
+  }
+}
