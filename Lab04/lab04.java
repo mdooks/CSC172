a52826578a70fafc8925c9156c71b5d8fc265c06
Nick Graham <nickdud341@gmail.com>
2015-02-02 22:17:25 -0500

    Whoops
    Broke unorderedSelection in the switch to BigInteger, all is good now.

--- Lab04/lab04.java
+++ Lab04/lab04.java
@@ -15,9 +15,14 @@ public class lab04{
 
     System.out.println("How many ways are there to form a sequence of m letters out of the 26 letters, if no letter is allowed to appear more than once for m=3? " + orderedSelection(new BigInteger("26"), new BigInteger("3")));
 
-System.out.println("How many ways are there to form a sequence of m letters out of the 26 letters, if no letter is allowed to appera more than once for m=5? " + orderedSelection(new BigInteger("26"), new BigInteger("5")));
+    System.out.println("How many ways are there to form a sequence of m letters out of the 26 letters, if no letter is allowed to appear more than once for m=5? " + orderedSelection(new BigInteger("26"), new BigInteger("5")));
 
-    System.out.println("In poker, each player is dealt five cards from a 52 card deck. How many different possible hands are there?  " + unorderedSelection(new BigInteger("25"), new BigInteger ("5")));
+    System.out.println("In poker, each player is dealt five cards from a 52 card deck. How many different possible hands are there?  " + unorderedSelection(new BigInteger("52"), new BigInteger ("5")));
+    //BigInteger n = new BigInteger("52");
+    //BigInteger m = new BigInteger("5");
+    //System.out.println(n.subtract(m).subtract(BigInteger.ONE));
+    //System.out.println(unorderedSelection(new BigInteger("52"), new BigInteger("5")));
+      //unorderedSelection(((n.subtract(m)).subtract(BigInteger.ONE)), BigInteger.ONE));
   }
 
   private static BigInteger assignments(BigInteger k, BigInteger n){
@@ -45,11 +50,12 @@ System.out.println("How many ways are there to form a sequence of m letters out 
   }
 
   private static BigInteger unorderedSelection (BigInteger n, BigInteger m){
-    if (m.intValue() == 0)
+    if (m.intValue() == 0 || n.equals(m)){
       return BigInteger.ONE;
-    else if(n.equals(m))
-      return BigInteger.ONE;
-    else
-      return unorderedSelection(n.subtract(BigInteger.ONE), m).add( unorderedSelection(n.subtract(BigInteger.ONE), m.subtract(BigInteger.ONE)));
+    }
+    else{
+      return (unorderedSelection(n.subtract(BigInteger.ONE), m.subtract(BigInteger.ONE)).add(unorderedSelection(n.subtract(BigInteger.ONE), m)));
+
+    }
   }
 }
