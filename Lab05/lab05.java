7a63c5f671163d32e6de7a186b9126a37e62b468
Nick Graham <nickdud341@gmail.com>
2015-02-03 20:39:19 -0500

    Finalized Lab 5
    Looks like all the math is right now, and everything is functional.
    need to implement a few more tests, and do the write up, but I think it
    is all set otherwise…

--- Lab05/lab05.java
+++ Lab05/lab05.java
@@ -6,13 +6,20 @@ public class lab05{
     System.out.println("There are " + newIdenticalOrder("street") + " anagrams of 'street'");
     System.out.println("There are " + newIdenticalOrder("allele") + " anagrams of 'allele'");
     System.out.println("There are " + newIdenticalOrder("Mississippi") + " anagrams of 'Mississippi'");
-    //System.out.println(bins(new BigInteger("6"), new BigInteger("4")));
-    //System.out.println(bins(new BigInteger("4"), new BigInteger("6")));
+    System.out.println(BinCombo(new BigInteger("4"), new BigInteger("3")));
+    System.out.println(BinCombo(new BigInteger("6"), new BigInteger("4")));
+    System.out.println(BinCombo(new BigInteger("4"), new BigInteger("6")));
+    System.out.println();
+    BigInteger[] list = new BigInteger[2];
+    list[0] = new BigInteger("6");
+    list[1] = new BigInteger("3");
+    System.out.println(distinguishableBins(list, new BigInteger("5")));
   }
 
   public static BigInteger newIdenticalOrder (String input){
-    long top = permutations(input.length());
-    long denom = 1;
+    //BigInteger len = BigInteger.valueOf(input.length());
+    BigInteger top = permutations(BigInteger.valueOf(input.length()));
+    BigInteger denom = BigInteger.ONE;
     char[] inChar = input.toCharArray();
     for (int i=0; i < inChar.length; i++){
       char c= inChar[i];
@@ -23,27 +30,44 @@ public class lab05{
           tot++;
         }
       }
-      denom *= tot;
+      denom.multiply(BigInteger.valueOf(tot));
     }
-    return BigInteger.valueOf(top/denom);
+    return (top.divide(denom));
   }
 
-  public static BigInteger bins (BigInteger items, BigInteger bins){
-    if (items.intValue() == 0 || bins.equals(items)){
-      return BigInteger.ONE;
-    }
-    else{
-      return (bins(items.subtract(BigInteger.ONE), bins.subtract(BigInteger.ONE)).add(bins(items.subtract(BigInteger.ONE), bins)));
-    }
+  public static BigInteger BinCombo (BigInteger items, BigInteger bins){
+    BigInteger n = items.add(bins).subtract(BigInteger.ONE);
+    BigInteger m = items;
+    return unorderedSelection(n,m);
     //return (permutations(items+bins-1))/(permutations(items));
   }
 
-  private static long permutations (int n){
-    if (n == 1)
-      return 1;
-    else if (n==0)
-      return 1;
+  private static BigInteger permutations (BigInteger n){
+    if (n.intValue() == 1)
+      return BigInteger.ONE;
+    else if (n.intValue()==0)
+      return BigInteger.ONE;
     else
-      return (n * permutations(n-1));
+      return (n.multiply(permutations(n.subtract(BigInteger.ONE))));
+  }
+  private static BigInteger unorderedSelection (BigInteger n, BigInteger m){
+    if (m.intValue() == 0 || n.equals(m)){
+      return BigInteger.ONE;
+    }
+    else{
+      return (unorderedSelection(n.subtract(BigInteger.ONE), m.subtract(BigInteger.ONE)).add(unorderedSelection(n.subtract(BigInteger.ONE), m)));
+
+    }
+  }
+
+  private static BigInteger distinguishableBins (BigInteger[] objects, BigInteger bins){
+    BigInteger tot = new BigInteger("0");
+    BigInteger dmul = BigInteger.ONE;
+    for (BigInteger o : objects){
+      tot = tot.add(o);
+      dmul = dmul.multiply(permutations(o));
+    }
+    return permutations(tot.add(bins).subtract(BigInteger.ONE)).divide(permutations(bins.subtract(BigInteger.ONE)).multiply(dmul));
+
   }
 }
