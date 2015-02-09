0454efbcc1c2509fda7d6bc69a75285d116d4673
Nick Graham <nickdud341@gmail.com>
2015-02-09 14:52:11 -0500

    Started project 1

---
+++ Project01/MasterMind.java
@@ -0,0 +1,15 @@
+public class MasterMind implements mm {
+  public MasterMind (String[] tokencolors, int positions){
+    
+  }
+  public void response(int colorsRightPositionWrong, int positionsAndColorRight) {
+
+  }
+  public void newGame() {
+
+  }
+  public String [] nextMove() {
+    String[] x = {"s", "d"};
+    return x;
+  }
+}
