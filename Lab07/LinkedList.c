80611f68dbe29961fec017edc9dee24d80e153b4
Nick Graham <nickdud341@gmail.com>
2015-02-09 21:11:13 -0500

    Finished up lab 7

--- Lab07/LinkedList.c
+++ Lab07/LinkedList.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
-int main(int argc, char * argv[]) {
+void mystrcat(char *dest, char *source);
+void mystrcpy(char *s, char *t);
+int mystrcmp(char *s, char *t);
+
+int main(int argc, char *argv[]) {
+  char msg[30];
+  mystrcpy(msg, "Hello, ");
   for (int len = 0; len < argc; len++){
-    printf("%s\n", argv[len]);
+    if (mystrcmp(argv[len], "Nick") == 0){
+      mystrcat(msg, argv[len]);
+      printf("%s\n", msg);
+    }
+    else{
+      printf("%s\n", argv[len]);
+    }
   }
+  int square[10];
+  for (int i=0; i < (int) (sizeof(square)/sizeof(int)); i++){
+    square[i] = i*i;
+  }
+
+  int *s = square;
+
+  for (int i = 0; i< (int) (sizeof(square)/sizeof(int)); i++){
+    printf("%d, ", *(s+i));
+  }
+  printf("\n");
+  return 0;
 
 }
 int mystrcmp(char *s, char *t){
-  int i;
-
   for ( ; *s == *t; s++, t++){
     if(*s == '\0'){
       return 0;
     }
   }
   return *s - *t;
+}
+void mystrcat(char *dest, char *source){
+  while(*dest != '\0'){
+    dest++;
+  }
+  while ((*dest = *source) != '\0'){
+    dest++;
+    source++;
+  }
 }
 void mystrcpy(char *s, char *t){
   int i;
