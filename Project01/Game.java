42a0c95255b5431797260378a6093e3fdd375282
Nick Graham <nickdud341@gmail.com>
2015-02-20 10:42:44 -0500

    Forgot the headers on the files. now it should be ok

--- Project01/Game.java
+++ Project01/Game.java
@@ -1,3 +1,24 @@
+/*
+* Project 1
+*
+* Project 1
+*
+* Copyright 2015
+*
+* Course: CSC 172 Spring 2015
+*
+* Assignment: Lab 3
+*
+* Author: Nicholas Graham
+*
+* Email: ngraham@u.rochester.edu
+*
+* Lab Session: Wednesday 6:15 - 7:30
+*
+* Lab TA: Kate Zeng Zhiming
+*
+* Last Revised: January 29, 2014.
+*/
 import java.util.Scanner;
 
 public class Game {
