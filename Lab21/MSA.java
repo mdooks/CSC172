12fdb25085e0a80f9373d4e50c4d506012211e76
Nick Graham <nickdud341@gmail.com>
2015-04-20 18:26:58 -0400

    Finished the Code for Lab 21

--- Lab21/MSA.java
+++ Lab21/MSA.java
@@ -8,7 +8,8 @@ public class MSA {
       System.out.println("Usage: java MSA <arraySize>");
       System.exit(1);
     }
-    Integer len = new Integer(args[1]);
+    Integer len = new Integer(args[0]);
+    System.out.println(len);
     double[] array = new double[len];
 
     for (int i = 0; i < len; i++){
@@ -20,22 +21,33 @@ public class MSA {
         sign = 1;
       }
       array[i] = rand.nextDouble() * 100 * sign;
-      System.out.println(array[i]);
+      //System.out.println(array[i]);
     }
 
+    System.out.println();
 
 
     long startTime = System.currentTimeMillis();
-    double subMax = 0;//call method here
+    double subMax = easyAns(array);//call method here
     long endTime = System.currentTimeMillis();
     long elapsedTime = endTime - startTime;
+
+    System.out.println("The max is: " + subMax + " and it took: " + elapsedTime + "ms to find with the easy way.");
+
+    startTime = System.currentTimeMillis();
+    subMax = divconq(array, 0, array.length - 1);//call method here
+    endTime = System.currentTimeMillis();
+    elapsedTime = endTime - startTime;
+
+    //System.out.println(subMax);
+    System.out.println("The max is: " + subMax + " and it took: " + elapsedTime + "ms to find with divide and conquor.");
   }
 
-  public static double easyAns(int[] arr){
+  public static double easyAns(double[] arr){
     double maxSum = 0;
     for (int i = 0; i < arr.length; i++){
-      for (int j = arr.length - 1; j >= 0; j++){
-        int sum = 0;
+      for (int j = arr.length - 1; j >= 0; j--){
+        double sum = 0;
         for (int k = i; k <= j; k++){
           sum += arr[k];
         }
@@ -47,7 +59,7 @@ public class MSA {
     return maxSum;
   }
 
-  public static double divconq(int[] arr, int lower, int upper){ //built from sudo-code given
+  public static double divconq(double[] arr, int lower, int upper){ //built from sudo-code given
     if (lower > upper){
       return 0;
     }
@@ -57,21 +69,21 @@ public class MSA {
     }
 
     int median = (lower + upper)/2;
-    int max_lower = 0;
-    int sum_lower = 0;
+    double max_lower = 0;
+    double sum_lower = 0;
     for (int i = median; i >= lower; i--){
       sum_lower += arr[i];
       max_lower = Math.max(sum_lower, max_lower);
     }
 
-    int max_upper = 0;
-    int sum_upper = 0;
-    for (int i = median + 1; i <= lower; i++){
+    double max_upper = 0;
+    double sum_upper = 0;
+    for (int i = median + 1; i <= upper; i++){
       sum_upper += arr[i];
       max_upper = Math.max(sum_upper, max_upper);
     }
 
-    int  cur_max = max_lower + max_upper;
+    double  cur_max = max_lower + max_upper;
 
     double rec_max_lower = divconq(arr, lower, median);
     double rec_max_upper = divconq(arr, median + 1, upper);
