3f6945cf4019a93110f45af9cde74bf81c3be161
Nick Graham <nickdud341@gmail.com>
2015-04-23 23:51:05 -0400

    Time test. Its all slow.
    This is working though so at least there is that.

--- Project04/Test.java
+++ Project04/Test.java
@@ -1,8 +1,13 @@
 public class Test {
   public static void main (String[] args){
-    Graph t = Graph.createFromFile("CSC172SP15_project4_data/nys.txt");
+    long s = System.currentTimeMillis();
+    Graph t = Graph.createFromFile("CSC172SP15_project4_data/monroe.txt");
+    long e = System.currentTimeMillis();
+    System.out.println(e-s);
     //t.shortPath("GOERGEN-ATHLETIC", "CSB");
     t.shortPath("i1", "i2");
+    long e2 = System.currentTimeMillis();
+    System.out.println(e2-s);
     //t.parentPrinter();
   }
 }
